83f7abe028c998c77721880eb9aa8948392cad52
Claud-B <b.m.claudia.sofia@gmail.com>
2024-11-29 09:47:04 +0000

    Introdução de função data de hoje, seguida de mensagem com o nome que se introduziu e indicação do ano, mês e dia correntes

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
+from datetime import date
 print("Olá turma do Python!")
 print("Tudo joia!")
 nome: str = input("Qual é o seu nome? ")
-print(f"Olá, {nome}!")
+
+data_hoje=date.today()
+print(f'Olá {nome}! Estamos no ano de', data_hoje.year, 'mês',data_hoje.month, 'dia', data_hoje.day'.')
