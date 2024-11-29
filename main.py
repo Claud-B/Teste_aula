0c7e431c0aec33f39b2a3cc64ea352c6d62be3c9
Claud-B <b.m.claudia.sofia@gmail.com>
2024-11-29 10:01:43 +0000

    Introduzido input para indicar temperatura no local, seguido de várias mensagens para diferentes temperaturas.

--- main.py
+++ main.py
@@ -5,3 +5,11 @@ nome: str = input("Qual é o seu nome? ")
 
 data_hoje=date.today()
 print(f'Olá {nome}! Estamos no ano de', data_hoje.year, 'mês',data_hoje.month, 'dia', data_hoje.day'.')
+
+temp=input('Indique a temperatura na sua localidade: ')
+if temp<15:
+    print('É melhor agasalhar-se.')
+elif temp==15:
+    print('15ºC ainda é considerado Primavera..Basta uma malhinha.')
+else:
+    print('Pode cancelar o voo para o Brasil. Vá para a Caparica.')
